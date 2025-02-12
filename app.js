c599255190bd0b97ab9d98efa36d60b2a118ea2c
Roysuchiro <suchirovk18@gmail.com>
2025-02-12 18:07:50 +0530

    Commit

--- app.js
+++ app.js
@@ -48,5 +48,5 @@ io.on('connection', (socket) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT
 server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
